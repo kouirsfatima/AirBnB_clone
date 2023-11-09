ef83f6c4454310340ed15c2fcfef3da5e49a1652
MD-MADARA <mouadotaku9421@gmail.com>
2023-11-09 13:02:10 +0100

    Place & Review & amenity

--- models/review.py
+++ models/review.py
@@ -1,1 +1,10 @@
 #!/usr/bin/python3
+from models.base_model import BaseModel
+""" module for Review class """
+
+
+class Review(BaseModel):
+    """ Review class """
+    place_id = ""
+    user_id = ""
+    text = ""
