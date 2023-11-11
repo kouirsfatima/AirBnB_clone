32e2b3f7a42dfad597f709359300fc1ba29529c4
MD-MADARA <mouadotaku9421@gmail.com>
2023-11-11 22:01:11 +0100

    add __init__.py to tests

---
+++ tests/test_models/test_base_model.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+""" unittest for BaseModel """
+import unittest
+from models.base_model import BaseModel
+from datetime import datetime
+
+
+class BaseModelTestCase(unittest.TestCase):
+    def test_BaseModel(self):
+        """attributes existance"""
+        new = BaseModel()
+        self.assertTrue(hasattr(new, "id"))
+        self.assertTrue(hasattr(new, "created_at"))
+        self.assertTrue(hasattr(new, "updated_at"))
+
+        """Methodes existance"""
+        self.assertTrue(hasattr(new, "__init__"))
+        self.assertTrue(hasattr(new, "__str__"))
+        self.assertTrue(hasattr(new, "save"))
+        self.assertTrue(hasattr(new, "to_dict"))
+
+        """type test"""
+        self.assertIsInstance(new.id, str)
+        self.assertIsInstance(new.created_at, datetime)
+        self.assertIsInstance(new.updated_at, datetime)
+    def test_str(self):
+        """test correct output for str method"""
+        new = BaseModel()
+        output = f"[{new.__class__.__name__}] ({new.id}) {new.__dict__}"
+        self.assertEqual(str(new), output)
