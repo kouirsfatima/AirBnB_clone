3128b00b16ed987d54f26e17e5efbd6b2a8e3fdf
MD-MADARA <mouadotaku9421@gmail.com>
2023-11-12 13:51:51 +0100

    fix test

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -1,19 +1,67 @@
 #!/usr/bin/python3
-""" unittest for test_file_storage """
 import unittest
+import json
 from models.engine.file_storage import FileStorage
+from models.base_model import BaseModel
+import models
+import os
 
 
-class FileStorage_TestCase(unittest.TestCase):
+class Test_FileStorage(unittest.TestCase):
+    """This Class for test FileStorage"""
 
-    def test_FileStorage(self):
-        storage = FileStorage()
-        self.assertTrue(hasattr(storage, "_FileStorage__file_path"))
-        self.assertTrue(hasattr(storage, "_FileStorage__objects"))
-        self.assertTrue(hasattr(storage, "all"))
-        self.assertTrue(hasattr(storage, "new"))
-        self.assertTrue(hasattr(storage, "save"))
-        self.assertTrue(hasattr(storage, "reload"))
+    def test_hasattr_and_type(self):
+        """test has attribute"""
+        obj = FileStorage()
 
-        self.assertIsInstance(storage._FileStorage__file_path, str)
-        self.assertIsInstance(storage._FileStorage__objects, dict)
+        """test has privet class attribute"""
+        self.assertTrue(hasattr(obj, "_FileStorage__file_path"))
+        self.assertTrue(hasattr(obj, "_FileStorage__objects"))
+
+        """test"""
+        _file = models.storage._FileStorage__file_path
+        _obj = models.storage._FileStorage__objects
+        self.assertEqual(_file, "file.json")
+        self.assertEqual(type(_file), str)
+        self.assertEqual(type(_obj), dict)
+
+    def test_all_models(self):
+        """Test all method"""
+        my_obj = BaseModel()
+
+        """test all"""
+        self.assertEqual(type(models.storage.all()), dict)
+        self.assertNotEqual(models.storage.all(), {})
+        """existence id"""
+        self.assertTrue(hasattr(my_obj, "id"))
+        self.assertEqual(type(my_obj.id), str)
+
+        """Test: my_obj"""
+        Cls_id = f"BaseModel.{my_obj.id}"
+        self.assertIsInstance(models.storage.all()[Cls_id], BaseModel)
+        self.assertEqual(models.storage.all()[Cls_id], my_obj)
+        """ Test: check if object exist by Cls_id """
+        self.assertIn(Cls_id, models.storage.all())
+        """ Test: check if the object found in storage with corrrect id"""
+        self.assertTrue(models.storage.all()[Cls_id] is my_obj)
+
+        """Test: save"""
+        models.storage.save()
+        with open("file.json", 'r') as file:
+            saved_data = json.load(file)
+        """ Test: check if object exist by Cls_id """
+        self.assertIn(Cls_id, saved_data)
+        """ Test: check if the value found in json is correct"""
+        self.assertEqual(saved_data[Cls_id], my_obj.to_dict())
+
+        """Test: reload"""
+        models.storage.all().clear()
+        models.storage.reload()
+        with open("file.json", 'r') as file:
+            saved_data = json.load(file)
+        self.assertEqual(saved_data[Cls_id],
+                         models.storage.all()[Cls_id].to_dict())
+
+
+if __name__ == '__main__':
+    unittest.main()
